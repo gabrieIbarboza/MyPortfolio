7792a4d343abd613ca718235fab8201d1a549451
Gabriel Klark <gabrielklarkgk@gmail.com>
2023-01-22 08:49:33 -0300

    Small fixes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-console.log('It is working!');
+(()=>{
+    console.log('It is working!');
+
+    setInterval(()=>{ 
+
+        function typeWriter(element) {
+            const textArray = (element.innerHTML.split(''));
+            element.innerHTML = '';
+            setTimeout(()=>{
+
+                textArray.forEach((letter, i) => {
+                    setTimeout(() => element.innerHTML += letter, 150 * i);
+                });
+
+            }, 150);
+        }
+
+        const title = document.querySelector('#siteTitle');
+        typeWriter(title);
+
+    }, 3000);
+
+})();
